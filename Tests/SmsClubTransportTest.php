0f2236d629a985701da79f3c0f5e858bd2acec78
kikasso <dakasso@gmail.com>
2025-09-07 19:51:13 +0300

    PHP CS Fixer

--- Tests/SmsClubTransportTest.php
+++ Tests/SmsClubTransportTest.php
@@ -1,4 +1,6 @@
 <?php
+
+declare(strict_types=1);
 /*
  * This file is part of the Sol.parts package.
  *
@@ -10,15 +12,15 @@
 
 namespace SolParts\SymfonySmsClubNotifier\Tests;
 
-use Symfony\Component\Notifier\Test\TransportTestCase;
+use SolParts\SymfonySmsClubNotifier\SmsClubTransport;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\JsonMockResponse;
-use SolParts\SymfonySmsClubNotifier\SmsClubTransport;
 use Symfony\Component\Notifier\Exception\LengthException;
 use Symfony\Component\Notifier\Exception\TransportException;
 use Symfony\Component\Notifier\Message\ChatMessage;
 use Symfony\Component\Notifier\Message\SentMessage;
 use Symfony\Component\Notifier\Message\SmsMessage;
+use Symfony\Component\Notifier\Test\TransportTestCase;
 use Symfony\Component\Notifier\Tests\Transport\DummyMessage;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
@@ -53,11 +55,11 @@ final class SmsClubTransportTest extends TransportTestCase
                 'info' => [
                     '1241725993' => '380931234567',
                 ],
-            ]
+            ],
         ], info: ['http_code' => 200]);
 
         $client = new MockHttpClient(static function (string $method, string $url, array $options) use ($response): ResponseInterface {
-            $body = json_decode($options['body'], true);
+            $body = \json_decode($options['body'], true);
             self::assertSame([
                 'src_addr' => 'sender',
                 'phone' => ['380931234567'],
@@ -81,8 +83,8 @@ final class SmsClubTransportTest extends TransportTestCase
         $response = new JsonMockResponse(body: [
             'success_request' => [
                 'add_info' => [
-                    '38093123456789' => 'Incorrect phone number'
-                ]
+                    '38093123456789' => 'Incorrect phone number',
+                ],
             ],
         ], info: ['http_code' => 200]);
 
@@ -103,9 +105,9 @@ final class SmsClubTransportTest extends TransportTestCase
         $response = new JsonMockResponse(body: [
             'success_request' => [
                 'add_info' => [
-                    'src_addr' => "Некоректне Альфа Ім'я"
-                ]
-            ]
+                    'src_addr' => "Некоректне Альфа Ім'я",
+                ],
+            ],
         ], info: ['http_code' => 400]);
 
         $client = new MockHttpClient(static fn (): ResponseInterface => $response);
@@ -130,7 +132,7 @@ final class SmsClubTransportTest extends TransportTestCase
 
     public function testInvalidSubjectWithLatinSymbols()
     {
-        $message = new SmsMessage('380931234567', str_repeat('z', 1522));
+        $message = new SmsMessage('380931234567', \str_repeat('z', 1522));
         $transport = new SmsClubTransport('authToken', 'sender', $this->createMock(HttpClientInterface::class));
 
         $this->expectException(LengthException::class);
@@ -141,7 +143,7 @@ final class SmsClubTransportTest extends TransportTestCase
 
     public function testInvalidSubjectWithCyrillicSymbols()
     {
-        $message = new SmsMessage('380931234567', str_repeat('z', 661).'Й');
+        $message = new SmsMessage('380931234567', \str_repeat('z', 661) . 'Й');
         $transport = new SmsClubTransport('authToken', 'sender', $this->createMock(HttpClientInterface::class));
 
         $this->expectException(LengthException::class);
